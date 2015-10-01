b8699baef834ac08840179caef5c0c60115a2f40
MantasDi <uzsidirbsiupats@gmail.com>
2015-10-02 00:59:53 +0300

    MySQL prisijungimo šablonas

---
+++ mysql.php
@@ -0,0 +1,13 @@
+<?php
+
+# MySQL server
+mysql_value ('server', 'localhost');
+
+# MySQL username
+mysql_value ('username', '');
+
+# MySQL password
+mysql_value ('password', '');
+
+# MySQL database
+mysql_value ('database', '');
